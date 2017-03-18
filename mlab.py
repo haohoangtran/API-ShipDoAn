ef4b9db8b421db2dfc3c6612a285f3bfb6ca0dcf
laptopTCC-PC\laptopTCC <phamdinhkhanh>
2017-03-18 13:56:11 +0700

    version2

--- mlab.py
+++ mlab.py
@@ -1,22 +1,18 @@
 import mongoengine
 
-# mongodb://<dbuser>:<dbpassword>@ds133290.mlab.com:33290/amita
-host = "ds133290.mlab.com"
-port = 33290
-db_name = "amita"
-username = "1"
-password = "1"
-
+host = "ds123370.mlab.com"
+port = 23370
+db_name = "khanhphamdinh"
+username = "admin"
+password = "admin"
 
 def connect():
-    mongoengine.connect(db_name, host=host, port=port, username=username, password=password)
+    mongoengine.connect(db= db_name, host = host,port = port,username = username,password = password)
 
-
-def list2json(l):
+def listjson(l):
     import json
     return [json.loads(item.to_json()) for item in l]
 
-
-def item2json(item):
+def itemjson(item):
     import json
     return json.loads(item.to_json())
